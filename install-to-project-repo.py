14bc5a5856464ff05586cf4056db614b0cab6a08
Paul Huygen <paul.huygen@huygen.nl>
2017-09-20 09:30:27 +0200

    remove builtin statement

--- install-to-project-repo.py
+++ install-to-project-repo.py
@@ -10,11 +10,6 @@
 # https://github.com/carchrae/install-to-project-repo
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
-from builtins import (
-         bytes, dict, int, list, object, range, str,
-         ascii, chr, hex, input, next, oct, open,
-         pow, round, super,
-         filter, map, zip)
 
 import os
 import re
